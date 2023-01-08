0eb97b8f4a3a3dfd9f45fd2d84fc7e93f6f1c186
iBookdrupal <ternguh@gmail.com>
2023-01-08 01:45:24 +0100

    modified: react router headers

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+export default function About(){
+	return(
+		<div className='about'>
+			<h2>About Us </h2>
+			<p>Lorem ipsum dolor sit, amet consectetur, adipisicing elit. Voluptates recusandae minus inventore error quas blanditiis nemo ab harum dolorem aut cupiditate beatae iste facilis repellendus quam, dolorum neque temporibus quis?
+			</p>
+
+			<p>Lorem ipsum dolor sit, amet consectetur, adipisicing elit. Voluptates recusandae minus inventore error quas blanditiis nemo ab harum dolorem aut cupiditate beatae iste facilis repellendus quam, dolorum neque temporibus quis?
+			</p>
+
+		</div>
+		)
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+export default function Home(){
+	return(
+		<div className='home'>
+			<h2>Welcome </h2>
+			<p>Lorem ipsum dolor sit, amet consectetur, adipisicing elit. Voluptates recusandae minus inventore error quas blanditiis nemo ab harum dolorem aut cupiditate beatae iste facilis repellendus quam, dolorum neque temporibus quis?
+			</p>
+
+			<p>Lorem ipsum dolor sit, amet consectetur, adipisicing elit. Voluptates recusandae minus inventore error quas blanditiis nemo ab harum dolorem aut cupiditate beatae iste facilis repellendus quam, dolorum neque temporibus quis?
+			</p>
+
+
+			<p>Lorem ipsum dolor sit, amet consectetur, adipisicing elit. Voluptates recusandae minus inventore error quas blanditiis nemo ab harum dolorem aut cupiditate beatae iste facilis repellendus quam, dolorum neque temporibus quis?
+			</p>
+
+		</div>
+		)
+}
